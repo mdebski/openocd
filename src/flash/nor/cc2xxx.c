9ad6cb6cdc3be9acb9f69846567929c5db3c2dda
Maciej Dębski <md319428@students.mimuw.edu.pl>
2015-11-24 16:00:43 +0100

    probe, info implementation

--- src/flash/nor/cc2xxx.c
+++ src/flash/nor/cc2xxx.c
@@ -28,7 +28,7 @@
  * CC2538 ROM
  * http://www.ti.com/lit/ug/swru333a/swru333a.pdf
  *
- * CC2538 Datasheet
+ * CC2538 Datasheet (chapter 8)
  * http://www.ti.com/lit/ug/swru319c/swru319c.pdf
  *
  * CC2538 TI driverlib documentation
@@ -37,8 +37,26 @@
 
 #define CC_FLASH_BASE       0x00200000
 #define CC_FLASH_TOP        0x0027FFFF
+#define CC_FLASH_PAGE_SIZE  0x800
+
+#define CC_FCTL_REG         0x400D3008
+#define CC_FADDR_REG        0x400D300C
+#define CC_FWDATA_REG       0x400D3010
+#define CC_DIECFG0_REG      0x400D3014
+
+#define CC_FCTL_ERASE       (1<<0)
+#define CC_FCTL_WRITE       (1<<1)
+#define CC_FCTL_ABORT       (1<<5)
+#define CC_FCTL_FULL        (1<<6)
+#define CC_FCTL_BUSY        (1<<7)
 
 #define ERROR_NYI ERROR_FAIL
+
+struct cc2xxx_flash_bank {
+  int probed;
+  uint16_t chip_id;
+  uint32_t flash_size_b;
+};
 
 /* flash bank cc2xxx <base> <size> 0 0 <target#> */
 FLASH_BANK_COMMAND_HANDLER(cc2xxx_flash_bank_command)
@@ -46,7 +64,16 @@ FLASH_BANK_COMMAND_HANDLER(cc2xxx_flash_bank_command)
 	if (CMD_ARGC < 6)
 		return ERROR_COMMAND_SYNTAX_ERROR;
 
-	return ERROR_NYI;
+  struct cc2xxx_flash_bank *cc2xxx_info = malloc(sizeof(struct cc2xxx_flash_bank));
+	if (!cc2xxx_info) {
+    LOG_WARNING("cannot allocate host memory");
+		return ERROR_FAIL;
+  }
+  bank->driver_priv = cc2xxx_info;
+
+  cc2xxx_info->probed = 0;
+
+	return ERROR_OK;
 }
 
 static int cc2xxx_protect_check(struct flash_bank *bank)
@@ -70,19 +97,110 @@ static int cc2xxx_write(struct flash_bank *bank, const uint8_t *buffer,
 	return ERROR_NYI;
 }
 
+static int cc2xxx_fetch_info(struct flash_bank *bank) {
+	struct target *target = bank->target;
+	struct cc2xxx_flash_bank *cc2xxx_info = bank->driver_priv;
+
+  uint32_t diecfg0;
+  int retval = target_read_u32(target, CC_DIECFG0_REG, &diecfg0);
+	if (retval != ERROR_OK) return retval;
+
+  cc2xxx_info->chip_id = (uint16_t) (diecfg0 >> 16);
+
+  switch ((diecfg0 & ((1<<6)|(1<<5)|(1<<4))) >> 4) {
+    case 0x04:
+      cc2xxx_info->flash_size_b = 512 * 1024; break;
+    case 0x03:
+      cc2xxx_info->flash_size_b = 384 * 1024; break;
+    case 0x02:
+      cc2xxx_info->flash_size_b = 256 * 1024; break;
+    case 0x01:
+      cc2xxx_info->flash_size_b = 128 * 1024; break;
+    case 0x00:
+      cc2xxx_info->flash_size_b = 64 * 1024; break;
+    default:
+      LOG_ERROR("Unknown flash size. diecfg0: 0x%08x", diecfg0);
+      return ERROR_FAIL;
+  }
+
+	return ERROR_OK;
+}
+
 static int cc2xxx_probe(struct flash_bank *bank)
 {
-	return ERROR_NYI;
+	struct cc2xxx_flash_bank *cc2xxx_info = bank->driver_priv;
+
+  int retval = cc2xxx_fetch_info(bank);
+	if (retval != ERROR_OK) return retval;
+
+  LOG_INFO("chip id: 0x%04hx", cc2xxx_info->chip_id);
+  LOG_INFO("flash size: %d bytes", cc2xxx_info->flash_size_b);
+
+  if(cc2xxx_info->flash_size_b % CC_FLASH_PAGE_SIZE != 0) {
+    LOG_ERROR("Incorrect flash or page size: %d or %d",
+        cc2xxx_info->flash_size_b, CC_FLASH_PAGE_SIZE);
+    return ERROR_FAIL;
+  }
+
+  bank->base = CC_FLASH_BASE;
+  bank->size = cc2xxx_info->flash_size_b;
+  // Do not touch the last page - it's Lock Bit & CCA Page
+  bank->num_sectors = (cc2xxx_info->flash_size_b / CC_FLASH_PAGE_SIZE) - 1;
+
+	bank->sectors = malloc(sizeof(struct flash_sector) * bank->num_sectors);
+	if (!bank->sectors) {
+    LOG_WARNING("cannot allocate host memory");
+		return ERROR_FAIL;
+  }
+
+  int i;
+	for (i=0;i<bank->num_sectors;++i) {
+		bank->sectors[i].offset = i * CC_FLASH_PAGE_SIZE;
+		bank->sectors[i].size = CC_FLASH_PAGE_SIZE;
+		bank->sectors[i].is_erased = -1;
+		bank->sectors[i].is_protected = 1;
+	}
+
+  cc2xxx_info->probed = 1;
+
+  return ERROR_OK;
 }
 
 static int cc2xxx_info(struct flash_bank *bank, char *buf, int buf_size)
 {
-	return ERROR_NYI;
+	struct cc2xxx_flash_bank *cc2xxx_info = bank->driver_priv;
+
+  int retval = cc2xxx_fetch_info(bank);
+	if (retval != ERROR_OK) return retval;
+
+  int wrote = 0;
+
+  switch (cc2xxx_info->chip_id) {
+   case 0xB964:
+    retval = snprintf(buf + wrote, buf_size - wrote, "TI CC2538");
+    if(retval < 0) return ERROR_FAIL;
+    wrote += retval;
+    break;
+   default:
+    retval = snprintf(buf + wrote, buf_size - wrote, "Unknown");
+    if(retval < 0) return ERROR_FAIL;
+    wrote += retval;
+    break;
+  }
+
+  retval = snprintf(buf + wrote, buf_size - wrote, " - %d KB", cc2xxx_info->flash_size_b / 1024);
+  if(retval < 0) return ERROR_FAIL;
+  wrote += retval;
+
+  return ERROR_OK;
 }
 
 static int cc2xxx_auto_probe(struct flash_bank *bank)
 {
-	return ERROR_NYI;
+	struct cc2xxx_flash_bank *cc2xxx_info = bank->driver_priv;
+	if (cc2xxx_info->probed)
+		return ERROR_OK;
+	return cc2xxx_probe(bank);
 }
 
 static const struct command_registration cc2xxx_exec_command_handlers[] = {
